5eed3792f5a906ba68f7e2decbd050e115ccdf68
Saiko <60376646+WolfOfDoom@users.noreply.github.com>
2024-11-04 01:12:08 -0600

    0.20.3

--- Parte2/script/zapapo.js
+++ Parte2/script/zapapo.js
@@ -1,7 +1,11 @@
 // Selecciona la imagen central
 const cuerpoCentral = document.getElementById('cuerpoCentral');
-const piernaIzquierda = document.getElementById('piernaS'); // Asegúrate de que la clase coincida
+
+const piernaIzquierda = document.getElementById('piernaS'); // Asegúrate de que el id coincida
 const cuerpoDurazno = document.getElementById('duraznO');
+const textoTerror = document.getElementById('textoTerror');
+const manoIzquierda = document.getElementById('manoIzq');
+const manoDerecha = document.getElementById('manoDer');
 
 // Define las rutas para las imágenes original y la de reemplazo
 const imagenOriginal = '../img/2.png';
@@ -21,6 +25,16 @@ setTimeout(() => {
         cuerpoCentral.src = imagenReemplazo;
         piernaIzquierda.classList.add('hidden'); // Oculta la pierna izquierda sin animación
         cuerpoDurazno.classList.add('hidden');
+
+        // Muestra las manos al pasar el cursor
+        manoIzquierda.classList.remove('hidden');
+        manoDerecha.classList.remove('hidden');
+
+        // Muestra el texto al pasar el cursor
+        textoTerror.classList.remove('hidden');
+        textoTerror.classList.add('temblor-activo');
+
+        //Canciones
         cancionMostrar.pause(); // Pausa la canción actual
         cancionOcultar.currentTime = 0; // Reinicia la segunda canción
         cancionOcultar.play(); // Reproduce la segunda canción
@@ -31,10 +45,22 @@ setTimeout(() => {
         cuerpoCentral.src = imagenOriginal;
         piernaIzquierda.classList.remove('hidden'); // Muestra la pierna izquierda
         cuerpoDurazno.classList.remove('hidden');
+
+        // Oculta las manos cuando el cursor se retira
+        manoIzquierda.classList.add('hidden');
+        manoDerecha.classList.add('hidden');
+
+        // Oculta el texto cuando el cursor se retira
+        textoTerror.classList.add('hidden');
+        textoTerror.classList.remove('temblor-activo');
+
+        //Canciones
         cancionMostrar.play(); // Reproduce la canción cuando se muestra la pierna
         cancionOcultar.pause(); // Pausa la segunda canción
         cancionMostrar.currentTime = 0; // Reinicia la primera canción
         cancionMostrar.play(); // Reproduce la primera canción
     });
-}, 20000); // Espera 20000 ms (20 segundos) para habilitar los eventos
+}, 7000); // Espera 20000 ms (20 segundos) para habilitar los eventos
 //Octubre 16
+
+
